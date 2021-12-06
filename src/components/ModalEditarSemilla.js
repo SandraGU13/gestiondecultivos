73aecbc913e0512c5803e74be2446d06db4611df
jalb850420 <70179616+jalb850420@users.noreply.github.com>
2021-12-06 00:22:57 -0500

    Se crean las funciones en Agregar y editar Semilla
    para capturar los datos de los formularios y enviarlo como JSON

--- src/components/ModalEditarSemilla.js
+++ src/components/ModalEditarSemilla.js
@@ -3,58 +3,72 @@ import ModalHeader from "./ModalHeader";
 import { Link } from "react-router-dom";
 
 function ModalEditarSemilla() {
-  return (
-    <div class="modal fade" id="editarSemillaModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="text-gray-800" id="exampleModalLabel">Editar semilla</h5>
-                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                    </button>
-                </div>
-                <div class="modal-body">
+    var enviar = (e) => {
+        e.preventDefault()
 
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label for="nombre">Nombre</label>
-                                <input type="text" class="form-control" id="nombre" placeholder=""/>
-                                {/*<!--<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>-->*/}
-                            </div>
+        const data = {
+            nombre: e.target.nombre.value,
+            cost_agua: e.target.cost_agua.value,
+            cost_semilla: e.target.cost_semilla.value,
+            cost_fert: e.target.cost_fert.value
+        }
+        console.log(data)
+
+    };
+    return (
+        <form onSubmit={enviar}>
+            <div className="modal fade" id="editarSemillaModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+                aria-hidden="true">
+                <div className="modal-dialog modal-dialog-centered modal-lg" role="document">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="text-gray-800" id="exampleModalLabel">Editar semilla</h5>
+                            <button className="close" type="button" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">×</span>
+                            </button>
                         </div>
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label for="precio">Costo de Agua</label>
-                                <input type="text" class="form-control" id="precio" placeholder=""/>
+                        <div className="modal-body">
+
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <label for="nombre">Nombre</label>
+                                        <input type="text" className="form-control" name={"nombre"} placeholder="" />
+                                        {/*<!--<small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>-->*/}
+                                    </div>
+                                </div>
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <label for="precio">Costo de Agua</label>
+                                        <input type="text" className="form-control" name={"cost_agua"} placeholder="" />
+                                    </div>
+                                </div>
                             </div>
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <label for="precio">Costo de Semilla</label>
+                                        <input type="text" className="form-control" name={"cost_semilla"} placeholder="" />
+                                    </div>
+                                </div>
+                                <div className="col-md-6">
+                                    <div className="form-group">
+                                        <label for="precio">Costo de Fertilizante</label>
+                                        <input type="text" className="form-control" name={"cost_fert"} placeholder="" />
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div className="modal-footer">
+                            <button className="btn btn-warning" type="submit">Editar</button>
+                            <button className="btn btn-secondary" type="button" data-dismiss="modal">Cancelar</button>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label for="precio">Costo de Semilla</label>
-                                <input type="text" class="form-control" id="precio" placeholder=""/>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label for="precio">Costo de Fertilizante</label>
-                                <input type="text" class="form-control" id="precio" placeholder=""/>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-                <div class="modal-footer">
-                    <Link class="btn btn-warning" to="predios.html">Editar</Link>
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancelar</button>
                 </div>
             </div>
-        </div>
-    </div>
-  );
+        </form>
+    );
 }
 
 export default ModalEditarSemilla;
